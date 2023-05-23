abef1163cf11acfd85e74e2040e6570d30862cf9
junhyungson <cybergeron89@gmail.com>
2023-05-23 14:45:16 -0700

    initial set up

---
+++ README.md
@@ -0,0 +1,5 @@
+# Typescriptt Mern project tutorial
+
+## what i've done so far
+1. initial set up
+2. folder structure basic
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>TS amazona</header>
+      <main></main>
+
+      <footer>All rights dongo </footer>
+    </div>
+  )
+}
+
+export default App
