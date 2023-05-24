bdb40c6e1f04086df7629cc91af193b0b658d31b
junhyungson <cybergeron89@gmail.com>
2023-05-24 10:26:44 -0700

    added bootstrap navbar, made main container responsive-ish

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,44 @@
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { sampleProducts } from './data'
-import './App.css'
 
 function App() {
 
   return (
-    <div>
-      <header>TS amazona</header>
+    <div className='d-flex flex-column vh-100'>
+      <header>
+        <Navbar bg="dark" variant='dark' expand="lg">
+          <Container>
+            <Navbar.Brand>tsamazona-tut</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className='nav-link'>Cart</a>
+            <a href="/signIn" className='nav-link'>Sign In</a>
+          </Nav>
+        </Navbar>
+      </header>
       <main>
-        <ul>
-          {
-            sampleProducts.map((p) => (
-              <li key={p.name}>
-                <img className="product-image" src={p.image} alt={p.name} />
-                <h2>{p.name}</h2>
-                <p>{p.description}</p>
-                <p>${p.price}</p>
-              </li>
-            ))
-          }
-        </ul>
+        <Container className='mt-3'>
+          <Row>
+            {
+              sampleProducts.map((p) => (
+                <Col key={p.name} sm={6} md={4} lg={3}>
+                  <img className="product-image" src={p.image} alt={p.name} />
+                  <h2>{p.name}</h2>
+                  <p>{p.description}</p>
+                  <p>${p.price}</p>
+                </Col>
+              ))
+            }
+          </Row>
+        </Container>
       </main>
-      <footer>All rights dongo </footer>
+
+      <footer>
+        <div className='text-center'>
+          reserved here
+        </div>
+      </footer>
+
     </div>
   )
 }
